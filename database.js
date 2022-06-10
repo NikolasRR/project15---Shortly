f323b34ba20c19971bbad024dbe906d5a5ecebb0
Nikolas <nkls99@gmail.com>
2022-06-10 16:32:37 -0300

    deploy setup

--- database.js
+++ database.js
@@ -3,18 +3,11 @@ import "dotenv/config";
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = process.env.SQL_PASSWORD;
-const host = 'localhost';
-const port = 5432;
-const database = process.env.DB_NAME;
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+};
 
-const db = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
+const db = new Pool(config);
 
 export default db;
